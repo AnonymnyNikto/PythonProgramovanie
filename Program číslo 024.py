e7514079698c2f8712257401f7d2df74f281684b
Anonymny Dusan <56343473+AnonymnyNikto@users.noreply.github.com>
2019-11-13 11:36:37 +0100

    Add files via upload

---
+++ Program číslo 024.py
@@ -0,0 +1,15 @@
+import tkinter
+import random
+
+c = tkinter.Canvas(width=800,height=600)
+c.pack()
+
+mena = "Jozko","Misko","Andrej","Jurko"
+
+for i in mena:
+    c.create_rectangle(x)
+
+
+
+
+canvas.blind_all("<Up>",hore)
