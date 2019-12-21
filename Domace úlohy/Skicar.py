c30006aab24732ee9d0280b9aea88b18699d2000
Anonymny Dusan <56343473+AnonymnyNikto@users.noreply.github.com>
2019-12-21 20:38:06 +0100

    Add files via upload

--- Domace úlohy/Skicar.py
+++ Domace úlohy/Skicar.py
@@ -2,44 +2,33 @@ import tkinter
 import random
 
 x_max, y_max = 800, 600
+bg = input("Farba pozadia: ")
 
-c = tkinter.Canvas(width = x_max, height = y_max, bg = "white")
+root = tkinter.Tk()
+root.title("Skicar")
+root.attributes("-topmost", True)
+
+c = tkinter.Canvas(width = x_max, height = y_max, bg = bg)
 c.pack()
 
 b = 30
+farba = ["red","blue","green","black"]
+x = x_max - 2*b
 kreslit = False
 guma = False
 gumovat = False
-farba = "Black"
+farba_kreslenia = "Black"
 farba_y = y_max - 2*b
 hrubka = 1
 
-def cervena():
-    global cervena_x
-    cervena_x = x_max - 2*b
-    c.create_rectangle(cervena_x,farba_y,cervena_x + 2*b,farba_y + 2*b, fill = "red")
+
+
+
+for i in range(4):
+    farba_stvorca = farba[i] 
+    c.create_rectangle(x,farba_y,x + 2*b,farba_y + 2*b, fill= farba_stvorca)
+    x = x - 2*b
     
-def modra():
-    global modra_x
-    modra_x = x_max - 4*b
-    c.create_rectangle(modra_x,farba_y,modra_x + 2*b,farba_y + 2*b, fill = "blue")
-
-def zelena():
-    global zelena_x
-    zelena_x = x_max - 6*b
-    c.create_rectangle(zelena_x,farba_y,zelena_x + 2*b,farba_y + 2*b,fill = "green")
-    
-
-def cierna():
-    global cierna_x
-    cierna_x = x_max - 8*b
-    c.create_rectangle(cierna_x,farba_y,cierna_x + 2*b,farba_y + 2*b, fill="black")
-
-cervena()
-zelena()
-cierna()
-modra()
-
 def hruba():
     global hruba_x
     hruba_x = 0
@@ -62,50 +51,61 @@ hruba()
 stredna()
 tenka()
 
-def guma():
+def guma_button():
     global guma_x
     guma_x = 6*b
     c.create_rectangle(guma_x,farba_y,guma_x + 2*b,farba_y + 2*b)
-    c.create_text(guma_x+b,farba_y+b,text = "guma")
+    c.create_text(guma_x+b,farba_y+b,text = "Guma")
 
-guma()
+def guma_vsetko():
+    global guma_vsetko_x
+    guma_vsetko_x = x_max - 2*b
+    c.create_rectangle(guma_vsetko_x, 0, guma_vsetko_x + 2*b, 2*b)
+    c.create_text(guma_vsetko_x+b,0+b, text = "Zmaz vsetko")
+    
+
+guma_button()
+guma_vsetko()
 
 
 
 def press(event):
-    global xx
-    global yy
-    global kreslit, farba, hrubka, guma, gumovat
-    x = event.x
-    y = event.y
-    xx, yy = x, y
+    global kreslit, farba_kreslenia, hrubka, guma, gumovat, xpress, ypress
+    x, y = event.x, event.y
+    xpress, ypress = x, y
     if guma == True:
         gumovat = True
     else:
         kreslit = True
     if y >= farba_y:
-            if x >= cervena_x:
-                farba = "red"
-                kreslit = True
-                guma = False
-            elif modra_x <= x < cervena_x:
-                farba = "blue"
-                kreslit = True
-                guma = False
-            elif zelena_x <= x < modra_x:
-                farba = "green"
-                kreslit = True
-                guma = False
-            elif cierna_x <= x < zelena_x:
-                farba = "black"
-                kreslit = True
-                guma = False
+            if x >= (x_max - 2*b):
+                farba_kreslenia = farba[0]
+                kreslit = False
+                gumovat = False
+            elif (x_max - 4*b) <= x < (x_max - 2*b):
+                farba_kreslenia = farba[1]
+                kreslit = False
+                gumovat = False
+            elif (x_max - 6*b) <= x < (x_max - 4*b):
+                farba_kreslenia = farba[2]
+                kreslit = False
+                gumovat = False
+            elif (x_max - 8*b) <= x < (x_max - 6*b):
+                farba_kreslenia = farba[3]
+                kreslit = False
+                gumovat = False
             elif stredna_x > x >= hruba_x:
                 hrubka = 10
+                kreslit = False
+                gumovat = False
             elif tenka_x >= x > stredna_x:
                 hrubka = 5
+                kreslit = False
+                gumovat = False
             elif guma_x >= x > tenka_x:
                 hrubka = 1
+                kreslit = False
+                gumovat = False
             elif guma_x+2*b >= x > guma_x:
                 if guma == False:
                     kreslit = False
@@ -113,6 +113,20 @@ def press(event):
                 else:
                     kreslit = True
                     guma = False
+    if y <= 2*b and x >= guma_vsetko_x :
+        c.delete("all")
+        x = x_max - 2*b
+        for i in range(4):
+            farba_stvorca = farba[i] 
+            c.create_rectangle(x,farba_y,x + 2*b,farba_y + 2*b, fill= farba_stvorca)
+            x = x - 2*b
+        hruba()
+        stredna()
+        tenka()
+        guma_button()
+        guma_vsetko()
+        kreslit = False
+        gumovat = False
 
 def release(event):
     global kreslit, gumovat
@@ -122,33 +136,25 @@ def release(event):
     
 
 def motion(event):
-    global kreslit, gumovat
+    global kreslit, gumovat, xpress, ypress
     if kreslit == True:
-        global xx
-        global yy
-        x = event.x
-        y = event.y
-        if y <= farba_y or guma_x + 2*b <= x <= cierna_x:
-            c.create_line(x,y,xx,yy,fill = farba,width = hrubka)
-            xx, yy = x, y
-        else:
-            kreslit = False
+        x, y = event.x, event.y
+        if y <= farba_y or guma_x + 2*b <= x <= x_max - 8*b:
+            if not (y <= 2*b and x >= guma_vsetko_x): 
+                c.create_line(x, y, xpress, ypress ,fill = farba_kreslenia, width = hrubka)
+                xpress, ypress = x, y
+            else:
+                kreslit = False
     elif gumovat == True:
-        x = event.x
-        y = event.y
-        if y <= farba_y or guma_x + 2*b <= x <= cierna_x:
-            c.create_oval(x + 2*hrubka, y + 2*hrubka, x - 2*hrubka, y - 2*hrubka, fill = "white", outline = "white")
+        x, y = event.x, event.y
+        if y <= farba_y - 2*hrubka or guma_x + 2*b + 2*hrubka <= x <= x_max - 8*b - 2*hrubka:
+            c.create_oval(x + 2*hrubka, y + 2*hrubka, x - 2*hrubka, y - 2*hrubka, fill = bg, outline = bg)
         else:
             gumovat = False
+
             
-        
     
-        
-
-
-        
     
-
 c.bind("<ButtonPress>",press)
 c.bind("<ButtonRelease>",release)
 c.bind("<Motion>",motion)
